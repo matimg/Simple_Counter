cbf2a40d938beb312623c007904ea1702121cd7a
Matías Gandini <matias.gandinimg@gmail.com>
2021-04-29 15:00:17 +0000

    cards dibujadas

---
+++ src/js/component/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = pops => {
+	return (
+		<div className="card bg-secondary">
+			<div className="card-body">
+				<p className="card-title text-white display-3">8</p>
+			</div>
+		</div>
+	);
+};
+
+export default Card;
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,33 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Card from "./Card.js";
 
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-5 px-5">
+			<div className="row p-5 bg-dark text-center">
+				<div className="col">
+					<Card />
+				</div>
+				<div className="col">
+					<Card />
+				</div>
+				<div className="col">
+					<Card />
+				</div>
+				<div className="col">
+					<Card />
+				</div>
+				<div className="col">
+					<Card />
+				</div>
+				<div className="col">
+					<Card />
+				</div>
+				<div className="col">
+					<Card />
+				</div>
+			</div>
 		</div>
 	);
 }
